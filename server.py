8dde8d937f36d5bd3b5e202f3b3de63edd9b35f6
Reganto <tell.reganto@gmail.com>
2022-05-17 17:24:48 +0430

    Initial commit

---
+++ server.py
@@ -0,0 +1,58 @@
+from tornado.web import Application, RequestHandler, url
+from tornado.ioloop import IOLoop
+from tornado.options import parse_command_line
+from tornado.websocket import WebSocketHandler
+
+
+class HomeHandler(RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+class ChatConnection(WebSocketHandler):
+    participants = set()
+
+    def send(self, participant, message):
+        participant.write_message(message)
+
+    def broadcast(self, participants, message):
+        for participant in participants:
+            self.send(participant, message)
+
+    def open(self):
+        self.broadcast(
+                self.participants,
+                "Someone joined to room",
+        )
+        self.participants.add(self)
+
+    def on_message(self, message):
+        self.broadcast(
+                self.participants,
+                message,
+        )
+    def on_close(self):
+        self.participants.remove(self)
+        self.broadcast(
+                self.participants,
+                "Someone leaved chat room",
+        )
+
+
+class App(Application):
+    def __init__(self):
+        handlers = [
+            url('/', HomeHandler, name='foo'),
+            url("/ws", ChatConnection, name="chat"),
+        ]
+        settings = dict(
+            debug=True,
+        )
+        super().__init__(handlers, **settings)
+
+
+if __name__ == '__main__':
+    parse_command_line()
+    App().listen(8001)
+    IOLoop.current().start()
+
